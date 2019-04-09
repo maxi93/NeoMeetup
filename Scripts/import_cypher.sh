1ba81c22d4cc5b3a5171c88b1a3f8d58971dfea7
DBertazioli <d.bertazioli@campus.unimib.it>
2019-04-09 16:20:50 +0000

    maybe a bit fixed but gotta try it

--- Scripts/import_cypher.sh
+++ Scripts/import_cypher.sh
@@ -38,25 +38,32 @@ echo "importing members"
 
 cat ${SCRIPT_DIR}/create_members_from_csv.cql |nohup bash -ci 'cypher' &
 
-echo "im here"
-for job in `jobs -p`
-do
-echo "job is " $job
-    wait $job || let "FAIL+=1"
-done
+wait `jobs -p` || let "FAIL+=1"
 
-echo "importing groups"
+cat ${SCRIPT_DIR}/groups_from_csv.cql |nohup bash -ci 'cypher' & >> out1.out
 
-cat ${SCRIPT_DIR}/groups_from_csv.cql |nohup bash -ci 'cypher' &
+wait `jobs -p` || let "FAIL+=1"
 
-echo "im here"
-
-for job in `jobs -p`
-do
-echo "job is " $job 
-    wait $job || let "FAIL+=1"
-done
-echo "im here"
+: '
+echo "importing events"
+cat ${SCRIPT_DIR}/events_from_csv.cql |bash -ci 'cypher'
+echo "importing venues"
+cat ${SCRIPT_DIR}/venues_from_csv.cql |bash -ci 'cypher'
+echo "importing topics"
+cat ${SCRIPT_DIR}/create_topics_from_csv.cql |bash -ci 'cypher'
+echo "importing relations between event and group"
+cat ${SCRIPT_DIR}/relation_event_group.cql |bash -ci 'cypher'
+echo "importing relations event-venue"
+cat ${SCRIPT_DIR}/relation_event_venue.cql |bash -ci 'cypher'
+echo "importing relations group-topics"
+cat ${SCRIPT_DIR}/relation_group_topics.cql |bash -ci 'cypher'
+echo "importing relations member-events"
+cat ${SCRIPT_DIR}/relation_member_event.cql |bash -ci 'cypher'
+echo "importing relations member-group"
+cat ${SCRIPT_DIR}/relation_member_group.cql |bash -ci 'cypher'
+echo "importing relations member-group"
+cat ${SCRIPT_DIR}/relation_member_topics.cql |bash -ci 'cypher'
+'
 
 if [ "$FAIL" == "0" ];
 then
