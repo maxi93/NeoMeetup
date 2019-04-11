82ca18c8870e7bafd6da302726c2021e9427c5c9
DBertazioli <d.bertazioli@campus.unimib.it>
2019-04-11 10:07:56 +0200

    member_enrich.py

--- Scripts/member_enrich.py
+++ Scripts/member_enrich.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[116]:
+
+
 from __future__ import unicode_literals
 
 import requests
@@ -5,44 +11,88 @@ import json
 import time
 import codecs
 import sys
+
+import pandas as pd
+
+
+# In[117]:
+
+
 UTF8Writer = codecs.getwriter('utf8')
 sys.stdout = UTF8Writer(sys.stdout)
 
+
+# In[118]:
+
+
+member_df=pd.read_csv("../Csv/Struttura/member.csv")
+#member_df.head()
+
+
+# In[119]:
+
+
+member_ids=member_df['member_id']
+member_try=[6,7]
+#member_ids.head()
+
+
+# In[120]:
+
+
+fabri_key="2e12625a12642d6ac743d19566c393e"
+
+
+# In[121]:
+
+
 def main():
-
-        members_id =
-        api_key= "YOUR_KEY_GOES_HERE"
+        #api_key= "2e12625a12642d6ac743d19566c393e"
+        #api_key=fabri_key
+        api_key= "546938372953301546964e404246e"
         # Get your key here https://secure.meetup.com/meetup_api/key/
-        for (city, state) in cities:
-            per_page = 200
-            results_we_got = per_page
+        for count,id_ in enumerate(member_ids):
+            per_page = 1
+            #results_we_got = per_page
             offset = 0
-            while (results_we_got == per_page):
-                # Meetup.com documentation here: http://www.meetup.com/meetup_api/docs/2/groups/
-                response=get_results({"sign":"true","country":"US", "city":city, "state":state, "radius": 10, "key":api_key, "page":per_page, "offset":offset })
-                time.sleep(1)
-                offset += 1
-                results_we_got = response['meta']['count']
-                for group in response['results']:
-                    category = ""
-                    if "category" in group:
-                        category = group['category']['name']
-                    print "," .join(map(unicode, [city, group['name'].replace(","," "), group['created'], group['city'],group.get('state',""),category,group['members'], group.get('who',"").replace(","," ")]))
+            # Meetup.com documentation here: http://www.meetup.com/meetup_api/docs/2/groups/
+            response=get_results({"member_id":id_, "key":api_key, "page":per_page, "offset":offset})
+            time.sleep(0.1)
+            offset += 1
+            #results_we_got = response['meta']['count']              
+            #time.sleep(1)
+            count+=1
+            if count is 20:
+                break
 
-            time.sleep(1)
+
+# In[122]:
 
 
 def get_results(params):
 
-	request = requests.get("http://api.meetup.com/2/groups",params=params)
+    request = requests.get("http://api.meetup.com/2/members",params=params)
     data = request.json()
-	
-	return data
+    #print(data)
+    #iter_data=json.load(data)
+    #a=data['results']
+    #print(a)
+    #for elem in a:
+        #print(elem['topics'])
+    print(request)
+    return data
+
+
+# In[123]:
 
 
 if __name__=="__main__":
         main()
 
 
+# In[ ]:
+
+
 ## Run this script and send it into a csv:
 ## python meetup-pages-names-dates.py > meetup_groups.csv
+
