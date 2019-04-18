e334579eb1cffdce7d809fb4c6f350a99717268b
maxi93 <m.perletti2@campus.unimib.it>
2019-04-18 13:33:56 +0000

    fix script and add groups csv

--- Scripts/make_group_csv.py
+++ Scripts/make_group_csv.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python
 import json
+import time
 import csv
 import ast
 from pprint import pprint
 from kafka import KafkaConsumer
 
+start_t=time.time()
+
 consumer = KafkaConsumer(bootstrap_servers = 'sandbox-hdf.hortonworks.com:6667',
                          auto_offset_reset = 'earliest',
                          consumer_timeout_ms = 1000)
-consumer.subscribe(['project'])
+consumer.subscribe(['project_april'])
 print("subscribed to topic meetup")
 messages = []
 groups= {}
@@ -32,9 +35,9 @@ for count, filename in enumerate(consumer):
        		print("outer for")
        		print e
 
-	if count==100000:
+	'''if count==100000:
 	        print "count is "+str(count)
-	        break
+	        break'''
           
 
 print "################"
@@ -43,9 +46,12 @@ print " names dict lenght: "+str(len(groups))
 
 #print groups
 #pprint(groups)
-with open('/root/csv/group.csv', 'wb') as f:  
+with open('/root/NeoMeetup/csv/group.csv', 'wb') as f:  
 	fields=["group_id","group_city","group_country","group_lat","group_lon","group_name","group_state","group_topics","group_urlname"]
 	w = csv.DictWriter(f, fields)
 	w.writeheader()
 	for k in groups:
 		w.writerow({field: groups[k].get(field) or "NONE" for field in fields})
+
+time=(time.time()-start_t)/60
+print "completed in "+str(time)+" minutes"
